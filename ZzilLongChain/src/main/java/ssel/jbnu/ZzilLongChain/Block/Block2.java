39e55f4646d212dfd44421b6c9c98938d09dc0bc
ZzillLongLee <rlaxodud1200@gmail.com>
2018-05-08 17:01:00 +0900

    bbbbbb

---
+++ ZzilLongChain/src/main/java/ssel/jbnu/ZzilLongChain/Block/Block2.java
@@ -0,0 +1,15 @@
+package ssel.jbnu.ZzilLongChain.Block;
+
+public class Block2 {
+	public Block2() {
+		
+	}
+	
+	public void getBlock() {
+		
+	}
+	
+	public void setBlock() {
+		
+	}
+}
---
+++ ZzilLongChain/src/main/java/ssel/jbnu/ZzilLongChain/Network/P2P/P2pNetWork.java
@@ -0,0 +1,12 @@
+package ssel.jbnu.ZzilLongChain.Network.P2P;
+
+public class P2pNetWork {
+
+	public P2pNetWork() {
+
+	}
+
+	public void connect() {
+		
+	}
+}
